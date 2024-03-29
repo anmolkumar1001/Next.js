6b7772d6b1bf198706b0cac4accd5f4112707763
Anmol Kumar <1RN20IS020@RNSIT.onmicrosoft.com>
2024-03-29 22:50:14 +0530

    till active link

---
+++ src/app/product/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+export default function ProductList() {
+  const productId = 100;
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <h1>Product List</h1>
+      <h2>
+        <Link href="product/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="product/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="product/3" replace>
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`product/${productId}`}>Product {productId}</Link>
+      </h2>
+    </>
+  );
+}
